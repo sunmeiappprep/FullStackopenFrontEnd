9a54db3e87e20366b5aaa6f954018547582cad42
Sunny Mei <sunmeiappprep@gmail.com>
2022-03-08 23:05:11 -0500

    moreq

--- test.js
+++ test.js
@@ -1,13 +1,34 @@
-function twoNumberSum(array, targetSum) {
-    let hash = {}
-    for (let i = 0; i < array.length; i++) {
-        const number = array[i];
-        const diff = targetSum-number
-        if(hash[diff]){
-            return [hash[diff],number]
-        }
-        hash[number] = number
-    }
-    return []
-}
+// function twoNumberSum(array, targetSum) {
+//     array.sort((a,b) => a - b)
+//     let l = 0
+//     let r = array.length - 1
 
+//     while (r > l){
+//         if (array[l]+array[r] === targetSum){
+//             return [array[l],array[r]]
+//         }
+//         else if (array[l]+array[r] < targetSum){
+//             l++
+//         }
+//         else{
+//             r--
+//         }
+//     }    
+//     return []
+// }
+
+
+// console.log(twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6],10))
+
+
+function isValidSubsequence(array, sequence) {
+    let arrayIdx = 0
+    let sequenceIdx = 0
+    while(arrayIdx < array.length && sequenceIdx < sequence.length){
+        if (array[arrayIdx] === sequence[sequenceIdx]) sequenceIdx++
+        arrayIdx++
+    }
+    return sequenceIdx === sequence.length
+  }
+
+console.log(isValidSubsequence([5, 1, 22, 25, 6, -1, 8, 10],[5, 1, 22, 22, 25, 6, -1, 8, 10]));
